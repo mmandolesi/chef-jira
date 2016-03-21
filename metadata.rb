65f7821477b761647b75165e269ef89ac8e2c9bc
Marcelo Mandolesi <mmandolesi@gmail.com>
2016-03-20 22:18:03 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'jira'
+maintainer 'Marcelo Mandolesi'
+maintainer_email 'mmandolesi@gmail.com'
+license 'all_rights'
+description 'Installs/Configures Atlassian Jira'
+long_description 'Installs/Configures Atlassian Jira'
+version '0.1.0'
